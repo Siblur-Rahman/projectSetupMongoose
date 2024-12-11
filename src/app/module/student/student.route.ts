b5d538023cf528dc7210de2d574586ecd36e7a66
MS Rahman <msrahman048gmail.com>
2024-12-11 09:35:52 +0600

    8-10 comleted

--- src/app/module/student/student.route.ts
+++ src/app/module/student/student.route.ts
@@ -4,5 +4,8 @@ import { StudentController } from './studentController';
 const router = express.Router();
 
 router.post('/create-student', StudentController.createStudent);
+router.get('/', StudentController.getAllStudents);
+
+router.get('/:studentId', StudentController.getSingleStudents);
 
 export const StudentRoutes = router;
--- src/app/module/student/studentController.ts
+++ src/app/module/student/studentController.ts
@@ -16,6 +16,36 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentService.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Students are recieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+const getSingleStudents = async (req: Request, res: Response) => {
+  try {
+    const studentId = req.params.studentId;
+    const result = await StudentService.getSingleStudentsFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is recieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const StudentController = {
   createStudent,
+  getAllStudents,
+  getSingleStudents,
 };
